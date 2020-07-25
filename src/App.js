27d320bf4ee6210ad373bc86ed5298b31ae4ce3b
Juan P. Frasser <frazzer_1004@hotmail.com>
2020-07-24 19:18:49 -0500

    maketado de photocard list con imagen por default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
 import { ListOfCategories } from "./components/listOfCategories";
+import { ListOfPhotoCards } from "./components/listOfPhotoCards";
 import { GlobalStyle } from "./GlobalStyles";
+
 export const App = () => (
   <Fragment>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </Fragment>
 );
