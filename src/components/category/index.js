ddb77f116dda0f643f1479e08f088c8117af73d1
Juan P. Frasser <frazzer_1004@hotmail.com>
2020-08-10 16:14:53 -0500

    se establece nuestra app con un comportamiento de single page app, utilizando las ventajas de Link mediante el reach router. ademas, se pone link al logo para llevar al home. el link reemplaza el anchor y este solamente le agrega el to en lugar del ref

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Anchor, Image } from "./styles";
+import { Link, Image } from "./styles";
 
 const DEFAULT_IMAGE =
   "http://samherbert.net/svg-loaders/svg-loaders/tail-spin.svg";
 
 export const Category = ({
   cover = DEFAULT_IMAGE,
-  path,
+  path = "#",
   emoji = "?",
   loading = true,
 }) => (
-  <Anchor href={path}>
+  <Link to={path}>
     <Image src={cover} loading={loading} />
     {emoji}
-  </Anchor>
+  </Link>
 );
