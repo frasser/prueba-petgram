1794dfd79a4c05ec687d8c31665e2fa179d81c12
Juan P. Frasser <frazzer_1004@hotmail.com>
2020-08-14 18:18:16 -0500

    se realizan ajustes a las rutas, se gestiona de mejor forma y con un codigo mas limpio las condicionales para el isAuth. ademas, se agrega un prop de noThrow en los redirect para evitar que se lance error en consola, lo cual es el manejo que le da reach-router a las excepciones de redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { Router } from "@reach/router";
+import React, { Fragment, useContext } from "react";
+import { Router, Redirect } from "@reach/router";
 
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/logo";
@@ -10,13 +10,15 @@ import { NavBar } from "./components/navBar";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisterUser";
+import { NotFound } from "./pages/NotFound";
 
-import Context from "./Context";
+import { Context } from "./Context";
 /*const Context.Consumer = ({ children }) => {
   return children({ isAuth: true });
 };*/
 
 export const App = () => {
+  const { isAuth } = useContext(Context);
   /*const urlParams = new window.URLSearchParams(window.location.search);
   const detailId = urlParams.get("detail");
   console.log(detailId);
@@ -32,26 +34,17 @@ export const App = () => {
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect from="/favs" to="/login" noThrow />}
+        {!isAuth && <Redirect from="/user" to="/login" noThrow />}
+        {isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </Fragment>
   );
