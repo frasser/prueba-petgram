3831b44f4c3fae143827993a1648df157b775c01
Juan P. Frasser <frazzer_1004@hotmail.com>
2020-08-14 00:55:46 -0500

    se puede hacer like como usuario registrado mediante el uso de web token

--- src/components/photoCards/index.js
+++ src/components/photoCards/index.js
@@ -1,19 +1,19 @@
-import React, { Fragment, useEffect, useRef, useState } from "react";
+import React, { Fragment } from "react";
 import { Article, ImgWrapper, Img } from "./styles";
 import { FavButton } from "../favButton";
 import { ToggleLikeMutation } from "../../container/ToggleLikeMutation";
 
-import { useLocalStorage } from "../../hooks/useLocalStorage";
+//import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
 import { Link } from "@reach/router";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element_ref] = useNearScreen();
-  const key = `like-${id}`;
-  const [liked, setLiked] = useLocalStorage(key, false);
+  //const key = `like-${id}`;
+  //const [liked, setLiked] = useLocalStorage(key, false);
 
   //console.log(liked);
 
@@ -29,13 +29,13 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           <ToggleLikeMutation>
             {(toggleLike) => {
               const handleFavClick = () => {
-                !liked &&
-                  toggleLike({
-                    variables: {
-                      input: { id },
-                    },
-                  });
-                setLiked(!liked);
+                //!liked &&
+                toggleLike({
+                  variables: {
+                    input: { id },
+                  },
+                });
+                //setLiked(!liked);
               };
 
               return (
